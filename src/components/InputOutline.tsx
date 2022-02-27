09a0a58c0bd3837812aab7bef63532e9f7ac9a86
John Edward Pedregosa <67826985+jedpedregosa@users.noreply.github.com>
2022-02-27 10:27:19 +0800

    [FIX] Height Issue

--- src/components/InputOutline.tsx
+++ src/components/InputOutline.tsx
@@ -8,7 +8,6 @@ import React, {
   useMemo,
 } from 'react';
 import {
-  Platform,
   StyleSheet,
   TextInput,
   TextInputProps,
@@ -353,9 +352,8 @@ const InputOutlineComponent = forwardRef<InputOutline, InputOutlineProps>(
       },
       inputContainer: {
         flex: 1,
+        paddingVertical,
         paddingHorizontal,
-        paddingVertical:
-          Platform.OS !== 'android' ? paddingVertical : undefined,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
